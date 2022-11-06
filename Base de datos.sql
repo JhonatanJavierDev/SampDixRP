b9a1508d5c9bcd4b8af993b76c15ac246334c177
Jhonatan Javier <74806674+eltiojhon7@users.noreply.github.com>
2022-11-06 17:21:25 -0300

    Update Base de datos.sql

--- Base de datos.sql
+++ Base de datos.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `276_vhrp`
+-- Base de datos: `276_mybd`
 --
 
 -- --------------------------------------------------------
